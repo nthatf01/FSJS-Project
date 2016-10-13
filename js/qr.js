bad32a2eb7cf9d131dbacb080eaaeadc17c5f440
nthatf01 <nthatfield@gmail.com>
2016-10-12 23:15:01 -0400

    Added answer checking functionality to qr.js.  Added a score counter and functionality to qr.js

--- js/qr.js
+++ js/qr.js
@@ -1,11 +1,38 @@
 var questionSet;
-var totalQuestions = -1;
+var currentQuestion;
+var qAnswer;
+var totalQuestions = 0;
+var correctlyAnswered = 0;
 
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
   if(xhr.readyState === 4) {
     questionSet = JSON.parse(xhr.responseText);
   }
+}
+
+//Handles Question and Answer functionality
+function questionHandler(){
+  //Increase Questions Asked counter
+  totalQuestions++;
+
+  //Capture User's Answer
+  var userAnswer = $("#answerBox").val();
+
+  //Check Answer
+  if (checkAnswer(userAnswer)) {
+    console.log("Correct!");
+    correctlyAnswered++;
+  }
+
+  //Update Score
+  updateScore();
+
+  //Clear #answerBox
+  answerBoxClear();
+
+  //Generate new Random Question
+  randomQuestion();
 }
 
 //Removes all Subject Area classes from the .questionContainer
@@ -18,13 +45,20 @@ function addSubjectClass(subjectClass){
   $(".questionContainer").addClass(subjectClass);
 }
 
-function randomQuestion(){
-  //Clear the text currently in the answerBox
+//Clear the  text in the #answerBox
+function answerBoxClear(){
   $("#answerBox").val('');
+}
 
+//Update the .score text
+function updateScore(){
+  $(".score").text(correctlyAnswered + " / " + totalQuestions);
+}
+
+function randomQuestion(){
   var randomQuestionNumber = Math.floor(Math.random() * questionSet.length);
+  qAnswer = questionSet[randomQuestionNumber].answer;
   var qSubject = questionSet[randomQuestionNumber].subject;
-  totalQuestions++;
   $(".question").text(questionSet[randomQuestionNumber].question);
 
   //Remove current Subject class
@@ -34,8 +68,14 @@ function randomQuestion(){
   addSubjectClass(qSubject);
 }
 
-function checkAnswer(){
-
+function checkAnswer(userAnswer){
+  var correctAnswer = false;
+  for(i = 0; i < qAnswer.length; i++) {
+    if(userAnswer.toUpperCase() === qAnswer[i]) {
+      correctAnswer = true;
+    }
+  }
+  return correctAnswer;
 }
 
 xhr.open("GET", "../data/questions.json");
@@ -45,15 +85,16 @@ $('#startButton').click(function(){
   $(this).hide();
   $("#answerBox").css("visibility", "visible");
   $("#answerButton").css("visibility", "visible");
+  $(".scoreContainer").css("visibility", "visible");
   randomQuestion();
 });
 
 $('#answerButton').click(function(){
-  randomQuestion();
+  questionHandler();
 });
 
 $('#answerBox').bind('keyup', function(e) {
     if ( e.keyCode === 13 ) { // 13 is enter key
-      randomQuestion();
+      questionHandler();
     }
 });
